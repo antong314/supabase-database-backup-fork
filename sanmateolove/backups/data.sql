a10323ab83f34db2c560a8f2c9baa3837cc10056
antong314 <antong314@users.noreply.github.com>
2026-03-10 00:12:52 +0000

    Supabase backup

--- sanmateolove/backups/data.sql
+++ sanmateolove/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict RP8kd1QvwZJar2kPXnjfL1WDcpcMLZcYEy4uOrPiIhbfmSoQu2C0e4diZ3ARbhx
+-- \restrict MqXL38q6MVMQtelXVsfaAsEDLquL7ch2ZCItOnSER4huVqZsy0SH176DbIiDVwl
 
 -- Dumped from database version 15.8
 -- Dumped by pg_dump version 17.6
@@ -524,6 +524,6 @@ SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, false);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict RP8kd1QvwZJar2kPXnjfL1WDcpcMLZcYEy4uOrPiIhbfmSoQu2C0e4diZ3ARbhx
+-- \unrestrict MqXL38q6MVMQtelXVsfaAsEDLquL7ch2ZCItOnSER4huVqZsy0SH176DbIiDVwl
 
 RESET ALL;
